6e37989aaed7ad83a2274702fa811d6b35036f56
krishna120801 <ksahu5505@gmail.com>
2019-12-29 20:29:38 +0530

    NEw Update

--- app/src/main/java/com/example/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -1,56 +1,68 @@
 package com.example.myapplication;
 import java.util.Random;
-import java.util.Scanner;
+import android.graphics.Color;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import android.view.View;
 import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
+    int attempts = 0;
+    final int maxAttempts = 3;
+    Random randGen = new Random();
+    int ranNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-    }
-    public class GussingGame
-    {
-        public void game(View view)
-        { int i;
+        final TextView textResponse = (TextView) findViewById(R.id.textView4);
+        final TextView guessText = (TextView) findViewById(R.id.textView5);
+        final EditText userGuess = (EditText) findViewById(R.id.editText);
 
-            Random rand= new Random();
-            int g=rand.nextInt(100);
-            int t=8;
-            Scanner input= new Scanner(System.in);
-            String message="";
-            System.out.println("Guess a number between 1 to 100:");
+        Button pressMe = (Button) findViewById(R.id.button5);
 
-            while(t>0)
-            {
-                System.out.println("\nEnter Your Guess Number \n");
-                i= input.nextInt();
-                if(i==g)
-                {
-                    message="Congragulation your guess is right You have won the game \n";
-                }
-
-                if(g>i)
-                {
-                    message="HINT: your guess is low \n";
-                }
-                if(g <i)
-                {
-                    message="HINT :Your guess is high \n";
-                }
-                t--;
-            }
-            message="\n\nYou have loose the game\n";
-              message="the nu. is " + "ParseInt(rand)";
-
-        }
+        randGen = new Random();
+        ranNum = randGen.nextInt(5);
+        pressMe.setOnClickListener(new View.OnClickListener()
+                                   {
+                                       public void onClick(View v) {
+                                           String randText = "";
+                                           Random randGen = new Random();
+                                           int ranNum = randGen.nextInt(5);
+                                           int userNumber = Integer.parseInt(userGuess.getText().toString());
 
 
-        EditText i=(EditText)findViewById(R.id.editText);
+                                           if (userNumber > 19) {
+                                               guessText.setText("Please guess between 0 and 20");
+                                           } else if (userNumber < ranNum) {
+                                               guessText.setText("Your answer is low. Guess again!");
+                                               guessText.setBackgroundColor(Color.CYAN);
+                                           } else if (userNumber > ranNum) {
+                                               guessText.setText("Your answer is high.  Guess again!");
+                                               guessText.setBackgroundColor(Color.RED);
+                                           } else if (userNumber == ranNum) {
+                                               ranNum = randGen.nextInt(5);
+                                               guessText.setText("You did it!");
+                                               guessText.setBackgroundColor(Color.WHITE);
+                                           }
+
+                                           while (ranNum != userNumber && ++attempts < maxAttempts) {
+
+                                               if (attempts == maxAttempts) {
+
+                                                   guessText.setText("You have guessed three times.  The answer was " + ranNum);
+                                              guessText.setBackgroundColor(Color.GREEN); }
+                                           }
+                                           randText = Integer.toString(ranNum);
+                                           textResponse.setText("");
+
+                                           userGuess.setText("");
+                                       }
+                                   }
+        );
     }
 }
+
